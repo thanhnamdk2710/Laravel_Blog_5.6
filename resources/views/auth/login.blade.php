758057b3cc67ccd99cf3c0ddb7fb3d74c17e515b
unknown <thanhnamdk2710@gmail.com>
2018-07-11 10:13:05 +0700

    Task Template Fontend

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,38 +1,52 @@
-@extends('layouts.app')
+@extends('layouts.frontend.app')
+
+@section('title', 'Login')
+
+@push('css')
+    <link href="{{ asset('assets/frontend/css/auth/styles.css') }}" rel="stylesheet">
+
+    <link href="{{ asset('assets/frontend/css/auth/responsive.css') }}" rel="stylesheet">
+@endpush
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+    <div class="slider display-table center-text">
+        <h1 class="title display-table-cell"><b>{{ __('LOGIN') }}</b></h1>
+    </div><!-- slider -->
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}" aria-label="{{ __('Login') }}">
-                        @csrf
-
+    <section class="blog-area section">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-2 col-md-0"></div>
+                <div class="col-lg-8 col-md-12">
+                    <div class="post-wrapper">
+                        {{ Form::open(['route' => 'login', 'aria-label' => __('Login')]) }}
                         <div class="form-group row">
-                            <label for="email" class="col-sm-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+                            {{ Form::label(
+                                'email', __('E-Mail Address'),
+                                ['class' => 'col-sm-4 col-form-label text-md-right'])
+                            }}
 
                             <div class="col-md-6">
-                                <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required autofocus>
+                                {{ Form::email('email', '', ['class' => 'form-control', 'autofocus']) }}
 
                                 @if ($errors->has('email'))
-                                    <span class="invalid-feedback" role="alert">
+                                    <span class="badge badge-warning" role="alert">
                                         <strong>{{ $errors->first('email') }}</strong>
                                     </span>
                                 @endif
                             </div>
                         </div>
-
                         <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
+                            {{ Form::label(
+                                'password', __('Password'),
+                                ['class' => 'col-sm-4 col-form-label text-md-right'])
+                            }}
 
                             <div class="col-md-6">
-                                <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
+                                {{ Form::password('password', ['class' => 'form-control']) }}
 
                                 @if ($errors->has('password'))
-                                    <span class="invalid-feedback" role="alert">
+                                    <span class="badge badge-warning" role="alert">
                                         <strong>{{ $errors->first('password') }}</strong>
                                     </span>
                                 @endif
@@ -43,7 +57,7 @@
                             <div class="col-md-6 offset-md-4">
                                 <div class="checkbox">
                                     <label>
-                                        <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> {{ __('Remember Me') }}
+                                        {{ Form::checkbox('remember', '') }} {{ __('Remember Me') }}
                                     </label>
                                 </div>
                             </div>
@@ -51,19 +65,21 @@
 
                         <div class="form-group row mb-0">
                             <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
+                                {{ Form::button(__('Login'), ['class' => 'btn btn-primary', 'type' => 'submit']) }}
 
                                 <a class="btn btn-link" href="{{ route('password.request') }}">
                                     {{ __('Forgot Your Password?') }}
                                 </a>
                             </div>
                         </div>
-                    </form>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
+                        {{ Form::close() }}
+                    </div><!-- post-wrapper -->
+                </div><!-- col-sm-8 col-sm-offset-2 -->
+            </div><!-- row -->
+        </div><!-- container -->
+    </section><!-- section -->
 @endsection
+
+@push('js')
+    <script src="{{ asset('assets/frontend/js/swiper.js') }}"></script>
+@endpush
