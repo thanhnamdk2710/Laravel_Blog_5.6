5694e5bfcc340a9e553edbe5583b3b21da4fe1ed
unknown <thanhnamdk2710@gmail.com>
2018-07-12 14:39:21 +0700

    Task Template Backend

---
+++ resources/views/layouts/backend/partials/topbar.blade.php
@@ -0,0 +1,26 @@
+<nav class="navbar">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <a href="javascript:void(0);" class="navbar-toggle collapsed" data-toggle="collapse"
+               data-target="#navbar-collapse" aria-expanded="false"></a>
+            <a href="javascript:void(0);" class="bars"></a>
+            @if(Request::is('admin*'))
+                <a class="navbar-brand" href="{{ route('admin.dashboard') }}">{{ __('ADMIN NAMDEV BLOG') }}</a>
+            @endif
+            @if(Request::is('author*'))
+                <a class="navbar-brand" href="{{ route('author.dashboard') }}">{{ __('AUTHOR NAMDEV BLOG') }}</a>
+            @endif
+        </div>
+        <div class="collapse navbar-collapse" id="navbar-collapse">
+            <ul class="nav navbar-nav navbar-right">
+                <!-- Call Search -->
+                <li>
+                    <a href="javascript:void(0);" class="js-search" data-close="true">
+                        <i class="material-icons">search</i>
+                    </a>
+                </li>
+                <!-- #END# Call Search -->
+            </ul>
+        </div>
+    </div>
+</nav>
